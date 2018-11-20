8bc41762ba5e156094c3718836c87aa1ff9670c9
mhahmed450 <mhahmed450@gmail.com>
2018-11-20 14:49:57 +0100

    Entity1

---
+++ src/main/java/com/ensi/ilsi/ParcManagement/Repository/Service/EquipementService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ensi.ilsi.ParcManagement.Repository.Service;
+
+import com.ensi.ilsi.ParcManagement.Web.EquipementRessource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.ensi.ilsi.ParcManagement.Repository.EquipementRepository;
+
+/**
+ *
+ * @author ahmed
+ */
+@Service
+@Transactional
+public class EquipementService {
+    private final Logger log= LoggerFactory.getLogger(EquipementService.class);
+    
+    //private final EquipementRepository equipementRepository ;
+    //private final Service service ;
+    
+    
+}
