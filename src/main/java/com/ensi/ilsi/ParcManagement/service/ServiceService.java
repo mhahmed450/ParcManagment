3caf7d376439f81cf6f4df8e8195aa19056d9053
mhahmed450 <mhahmed450@gmail.com>
2018-11-27 20:27:32 +0100

    V17-ahmed

--- src/main/java/com/ensi/ilsi/ParcManagement/service/ServiceService.java
+++ src/main/java/com/ensi/ilsi/ParcManagement/service/ServiceService.java
@@ -7,15 +7,21 @@ package com.ensi.ilsi.ParcManagement.service;
 
 
 import com.ensi.ilsi.ParcManagement.entity.Service;
+import com.ensi.ilsi.ParcManagement.repository.OfficeRepository;
 
 import com.ensi.ilsi.ParcManagement.repository.ServiceRepository;
+import com.ensi.ilsi.ParcManagement.web.dto.ServiceDto;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
- * @author nouha
+ * @author ahmed
  */
 
 @org.springframework.stereotype.Service
@@ -23,33 +29,63 @@ import org.springframework.transaction.annotation.Transactional;
 public class ServiceService {
         
     private final ServiceRepository serviceRepository;
+    private final OfficeRepository officeRepository;
+    
+    
+    private final Logger log = LoggerFactory.getLogger(ServiceService.class);
 
-    public ServiceService(ServiceRepository serviceRepository)
+    public ServiceService(ServiceRepository serviceRepository ,OfficeRepository officeRepository )
     {
      this.serviceRepository=serviceRepository;
+     this.officeRepository=officeRepository;
     }
 
     
-     public List<Service> findAll() {
-         return serviceRepository.findAll();
+     public List<ServiceDto> findAll() {
+          log.debug("Request to get all Services");
+        return this.serviceRepository.findAll()
+                .stream()
+                .map(ServiceService::mapToDto)
+                .collect(Collectors.toList());
+        
          
      }
      
 
 
-    public Optional<Service> findById(Long id) {
-         return this.serviceRepository.findById(id);
+    public Optional<ServiceDto> findById(Long id) {
+        
+           log.debug("Request to get Category : {}", id);
+        return this.serviceRepository.findById(id).map(ServiceService::mapToDto);
+    
         
     }
     
-    public Service create(Service service){
+    public ServiceDto create(ServiceDto serviceDto){
+        
+             log.debug("Request to create Service : {}", serviceDto);
+        return mapToDto(this.serviceRepository.save(
+                new Service(serviceDto.getName(), Collections.emptySet()) 
+                      
+                )
+        );
     
-        return  this.serviceRepository.save(service);
+        
     }
 
     public void delete(Long id) {
-        
+          log.debug("Request to delete service : {}", id);
         this.serviceRepository.deleteById(id);
+    }
+    
+    public static ServiceDto mapToDto(Service service){
+        if (service!=null){
+            return new ServiceDto(service.getServiceNumber(),
+                                    service.getName(),
+                                  service.getOffices().stream().map(ServiceOffice::mapToDto).collect(Collectors.toSet())        }
+        
     }
 
 }
+
+
